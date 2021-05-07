3c1aad32467874bfd6a47453aa2b1c2533bfe9bd
chinguyendac9974 <chinguyendac9974@gmail.com>
2021-05-07 09:31:25 +0700

    Add Time and Sort Time

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,19 +3,22 @@ import TodoList from './TodoList'
 import TodoForm from './TodoForm'
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import EditIcon from '@material-ui/icons/Edit';
+import * as _ from "lodash";
 
 function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     const [edit, setEdit] = useState({
         id: null,
         value: '',
+        time: '',
     })
 
 
-    const submitUpdate = value => {
-        updateTodo(edit.id, value);
+    const submitUpdate = (value,time) => {
+        updateTodo(edit.id, value,time);
         setEdit({
             id: null,
             value: '',
+            time: '',
         })
     }
 
@@ -23,17 +26,19 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
         return <TodoForm edit={edit} onSubmit={submitUpdate} />
     }
 
-    return todos.map((todo, index) => {
+    const sortTodos = _.sortBy(todos, 'time');
+
+    return sortTodos.map((todo, index) => {
         return <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
         key={index}>
             <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-                {todo.text}
+            {todo.time} : {todo.text} 
             </div>
             <div className="icons">
                 <HighlightOffIcon onClick={() => removeTodo(todo.id)}
                     className='delete-icon'
                 />
-                <EditIcon onClick={() => setEdit({id: todo.id, value: todo.text })}
+                <EditIcon onClick={() => setEdit({id: todo.id, value: todo.text, time: todo.time })}
                     className='edit-icon'
                 />
             </div>
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,25 +1,58 @@
 import React, { useState, useEffect, useRef } from "react";
+import PickTime from './PickTime';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 'auto',
+    color: 'white',
+    border: '2px solid #5d0cff',
+  },
+  labelModify: {
+    color: 'white',
+    top: 3,
+    left: 3
+  }
+}));
 
 function TodoForm(props) {
   console.log(props);
+  const classes = useStyles();
   const [input, setInput] = useState(props.edit ? props.edit.value : "");
+  const [time,setTime] = useState(props.edit ? props.edit.time : "07:30");
 
   const inputRef = useRef(null);
+  const timeRef = useRef(null);
 
   useEffect(() => {
     inputRef.current.focus();
   });
 
+  //Change Input
   const handleChange = (e) => {
     setInput(e.target.value);
   };
 
+  //Change Time
+  const handleChangeTime = (e) => {
+    setTime(e.target.value);
+  }
+
+  //Xử lý submit form
   const handleSubmit = (e) => {
     e.preventDefault();
 
     props.onSubmit({
       id: Math.floor(Math.random() * 10000),
       text: input,
+      time: time,
     });
 
     setInput("");
@@ -38,6 +71,22 @@ function TodoForm(props) {
             onChange={handleChange}
             ref={inputRef}
           />
+          <TextField
+            id="time"
+            label="Alarm clock"
+            type="time"
+            defaultValue={timeRef}
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true,
+              className: classes.labelModify,
+            }}
+            inputProps={{
+              step: 300, // 5 min
+            }}
+            value={time}
+            onChange={handleChangeTime}
+          />
           <button className="todo-button edit">Update</button>
         </>
       ) : (
@@ -50,6 +99,22 @@ function TodoForm(props) {
             className="todo-input"
             onChange={handleChange}
             ref={inputRef}
+          />
+          <TextField
+            id="time"
+            label="Alarm clock"
+            type="time"
+            defaultValue="07:30"
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true,
+              className: classes.labelModify,
+            }}
+            inputProps={{
+              step: 300, // 5 min
+            }}
+            value={time}
+            onChange={handleChangeTime}
           />
           <button className="todo-button">Add todo</button>
         </>
